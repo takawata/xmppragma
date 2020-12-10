74f8ae7b43dc9d869af5efb2acc5e27620711fd8
Takanori Watanabe <takawata@init-main.com>
2020-12-10 16:42:38 +0900

    Add loop construct parse.

--- src/main.cpp
+++ src/main.cpp
@@ -65,6 +65,8 @@ public:
 	(align_stmt->getSubExpr());
       auto align_decl = declref->getDecl();
       align_decl->dump();
+      rew.InsertTextBefore(vdecl->getBeginLoc(),
+			   "/* Pragma Align Found */");
     }
     llvm::errs()<<vdecl->getName()<<"\n";
     return true;
@@ -125,7 +127,16 @@ static void AddVar(clang::Preprocessor &PP,
   TokenList.push_back(Tok);
   
 }
-
+static void CreateUIntToken(clang::Preprocessor &PP,
+			    clang::Token &Tok, unsigned int val,
+			    clang::SourceLocation Start,
+			    clang::SourceLocation End)
+{
+  std::string vstr = std::to_string(val);
+  Tok.startToken();
+  Tok.setKind(clang::tok::numeric_constant);
+  PP.CreateString(vstr.c_str(), Tok, Start, End);
+}
 static void SetNumericConstant(clang::Token &Tok, const char *number)
 {
   Tok.startToken();
@@ -485,7 +496,10 @@ public:
     std::vector<int> arraykind;
     std::string idstr;
     std::string name;
-    clang::SourceLocation StartLoc = FirstTok.getLocation();        
+    int arraydim = 0;
+    int tempdim = 0;
+    clang::SourceLocation StartLoc = FirstTok.getLocation();
+    clang::SourceLocation EndLoc;
     name = std::string("__xmp_align") + std::to_string(nodes);    
     PP.Lex(alignTok);
     if(!alignTok.is(expected = clang::tok::identifier)){
@@ -493,13 +507,16 @@ public:
     }
     PP.Lex(Tok);
     while(Tok.is(clang::tok::l_square)){
+      arraydim ++;
       PP.Lex(Tok);
       if(Tok.is(clang::tok::star)){
 	arraykind.push_back(1);
-      } else  if(!Tok.is(expected = clang::tok::identifier)){
-	goto error;
+      }else if(Tok.is(clang::tok::colon)){
+	arraykind.push_back(2);
+      } else if(Tok.is(expected = clang::tok::identifier)){
+	arraykind.push_back(0);
       }else{
-	arraykind.push_back(0);	
+	goto error;
       }
       PP.Lex(Tok);
       if(!Tok.is(expected = clang::tok::r_square)){
@@ -518,22 +535,45 @@ public:
     if(!tempTok.is(expected = clang::tok::identifier)){
       goto error;
     }
-    
+
+    PP.Lex(Tok);
     while(Tok.is(clang::tok::l_square)){
+      int offs = 0;
       PP.Lex(Tok);
-      if(Tok.is(clang::tok::star)){
-	arraykind.push_back(1);
-      } else  if(!Tok.is(expected = clang::tok::identifier)){
-	goto error;
-      }else{
-	arraykind.push_back(0);	
+      if(tempdim >=arraydim){
+	if(!Tok.is(expected = clang::tok::star)){
+	  goto error;
+	}
+	PP.Lex(Tok);
+	if(!Tok.is(expected = clang::tok::r_square)){
+	  goto error;
+	}
+      }else if(Tok.is(clang::tok::identifier) && arraykind[tempdim] == 0){
+	PP.Lex(Tok);
+	while(!Tok.is(clang::tok::r_square)){
+	  //TODO: evaluate token to r_square
+	  PP.Lex(Tok);
+	}
+	arraykind[tempdim] =  offs;
+      }else if(Tok.is(clang::tok::colon)&& arraykind[tempdim] == 2){
+	PP.Lex(Tok);
+	if(!Tok.is(expected = clang::tok::r_square)){
+	  goto error;
+	}
+	arraykind[tempdim] =  offs;
       }
-      PP.Lex(Tok);      
-      if(!Tok.is(expected = clang::tok::r_square)){
-	goto error;
-      }
+      tempdim ++;
       PP.Lex(Tok);
     }
+
+    /* check for excess element*/
+    for(int i = tempdim; i < arraydim; i++){
+      if(arraykind[i] != 1){
+	expected = clang::tok::star;
+	goto error;
+      }
+    }
+    llvm::errs()<<arraydim<<","<<tempdim<<"\n";
     /*Discard tokens to eod*/
     while(!Tok.is(clang::tok::eod)){
       PP.Lex(Tok);
@@ -544,6 +584,24 @@ public:
       AddVar(PP, TokenList, name, StartLoc);
       AddTokenPtrElem(TokenList, alignTok);
       AddTokenPtrElem(TokenList, tempTok);
+      CreateUIntToken(PP, Tok, arraydim, StartLoc, EndLoc);
+      AddVoidCastToken(TokenList, Tok);
+      Tok.startToken();
+      Tok.setKind(clang::tok::comma);
+      TokenList.push_back(Tok);
+      CreateUIntToken(PP, Tok, tempdim, StartLoc, EndLoc);
+      AddVoidCastToken(TokenList, Tok);
+      Tok.startToken();
+      Tok.setKind(clang::tok::comma);
+      TokenList.push_back(Tok);
+
+      for(int i = 0; i < std::min(arraydim, tempdim) ; i++){
+	CreateUIntToken(PP, Tok, arraykind[i], StartLoc, EndLoc);
+	AddVoidCastToken(TokenList, Tok);
+	Tok.startToken();
+	Tok.setKind(clang::tok::comma);
+	TokenList.push_back(Tok);
+      }
       AddEndBrace(TokenList);
       /* other params are not concerned for now*/
       auto TokenArray = std::make_unique<clang::Token[]>(TokenList.size());
@@ -569,25 +627,98 @@ public:
   void HandlePragma(clang::Preprocessor &PP,
 		    clang::PragmaIntroducer Introducer,
 		    clang::Token &FirstTok) override{
-    clang::Token alignTok;
-    clang::Token tempTok;
     clang::Token Tok;
     clang::tok::TokenKind expected;
-    clang::SmallVector<clang::Token,1>  TokenList;  
-    std::vector<int> arraykind;
-    std::string idstr;
+    clang::SmallVector<clang::Token,1>  TokenList;
+    clang::SmallVector<clang::Token,1> LoopVarList;
+    std::vector<std::pair<clang::Token,clang::Token>> arrays;
+    clang::SourceLocation StartLoc = FirstTok.getLocation();
+    clang::SourceLocation EndLoc;
     std::string name;
-    clang::SourceLocation StartLoc = FirstTok.getLocation();        
-    name = std::string("__xmp_loop") + std::to_string(nodes);    
+
+    int hasvarlist;
+    name = std::string("__xmp_loop") + std::to_string(nodes);
+    PP.Lex(Tok);
+
+    if(Tok.is(clang::tok::l_paren)){
+      /*Parse loop var list*/
+      hasvarlist = 1;
+      for(;;){
+	PP.Lex(Tok);
+	if(!Tok.is(expected = clang::tok::identifier)){
+	  goto error;
+	}
+	LoopVarList.push_back(Tok);
+	PP.Lex(Tok);
+	if(Tok.is(clang::tok::r_paren)){
+	  break;
+	}else if(Tok.is(expected = clang::tok::comma)){
+	  goto error;
+	}
+      }
+    }
+    PP.Lex(Tok);
+    /* on */
+    if(!Tok.is(expected = clang::tok::identifier)){
+      goto error;
+    }
+
+    if(Tok.getIdentifierInfo()->getName().str() != "on"){
+      goto error;
+    }
+    /* node or template ref*/
+    PP.Lex(Tok);
+    if(!Tok.is(expected = clang::tok::identifier)){
+      goto error;
+    }
+    PP.Lex(Tok);
+    while(Tok.is(clang::tok::l_square)){
+      PP.Lex(Tok);
+      if(!Tok.is(expected = clang::tok::identifier)){
+	goto error;
+      }
+      if(hasvarlist){
+	LoopVarList.push_back(Tok);
+      }
+
+      PP.Lex(Tok);
+      if(!Tok.is(expected = clang::tok::r_square)){
+	goto error;
+      }
+
+    }
     /*Discard tokens to eod*/
     while(!Tok.is(clang::tok::eod)){
       PP.Lex(Tok);
     }
-    llvm::errs()<<"HOGEHOGE\n";
+
     /*Construct void * array*/
     {
+      /**/
+      Tok.startToken();
+      Tok.setKind(clang::tok::l_brace);
+      TokenList.push_back(Tok);
+
+      /*define loop variable decl.*/
+
+      /*Add descriptor array decl.*/
       AddVar(PP, TokenList, name, StartLoc);
       AddEndBrace(TokenList);
+      //Scan For loop
+      int forlevel = 0;
+      do{
+	PP.Lex(Tok);
+	/*for(int i=0; ...){} -> {int i; for(i=0; ...){}} */
+#if 0
+	if(Tok.is(clang::tok::kw_for)){
+	  forlevel ++;
+	}
+#endif
+	TokenList.push_back(Tok);
+      }while(forlevel > 0);
+      Tok.startToken();
+      Tok.setKind(clang::tok::r_brace);
+      TokenList.push_back(Tok);
       /* other params are not concerned for now*/
       auto TokenArray = std::make_unique<clang::Token[]>(TokenList.size());
       std::copy(TokenList.begin(), TokenList.end(), TokenArray.get());
