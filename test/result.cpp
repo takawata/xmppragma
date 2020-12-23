e4e20ab355aa3833232fb8f3feed3c96001e3f2f
Takanori Watanabe <takawata@init-main.com>
2020-12-23 14:45:31 +0900

    Add result processed by this tool.

---
+++ test/result.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#define N1 64
+#define N2 64
+
+void *p;
+static int __XMP_NODES_SIZE_p1;
+static int __XMP_NODES_RANK_p0;
+static int __XMP_NODES_RANK_p1;
+/*XMP_init_nodes_DYNAMIC_GLOBAL(&p,2);*/
+
+#pragma xmp template t[N2][N1]
+#pragma xmp distribute t[block][block] onto p
+
+double u[N2][N1];
+#pragma xmp /* Pragma Align Found */align u[j][i] with t[j][i]
+
+/* found during AST traversal */
+int main(int ac, char** av)
+{
+  /*confirm this is not replaced.*/
+  {
+    double u[2][3];
+    u[0][0] = 0.3;
+  }
+ 
+  printf("hello\n");
+#pragma xmp loop (j,i) on t[j][i]  
+  for(int j = 0; j < N2; j++){
+    for(int i = 0;i < N1; i++){
+      *u = 0.0;
+    }
+  }
+  return 0;
+}
+
