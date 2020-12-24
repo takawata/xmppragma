ecb728c1562002f67cfbc9a6e8afb58ddd9cdbfe
Takanori Watanabe <takawata@init-main.com>
2020-12-24 16:39:01 +0900

    Statisize node descriptor variable. Use Rewriter utility.

--- src/NodeHandler.cpp
+++ src/NodeHandler.cpp
@@ -4,18 +4,10 @@ bool MyASTVisitor::NodeHandler(clang::VarDecl *vdecl)
 {
 	clang::Expr::EvalResult ev;
 	clang::SourceLocation SL;
-	
-	llvm::errs()<<"NODE"<<"\n";
 	auto content = llvm::dyn_cast<clang::InitListExpr>(vdecl->getInit());
 	assert((content));
-	auto node_stmt = llvm::dyn_cast<clang::UnaryOperator>
-		(content->getInit(0)->IgnoreCasts());
-	auto declref = llvm::dyn_cast<clang::DeclRefExpr>
-		(node_stmt->getSubExpr());
-	auto node_decl = declref->getDecl();
-	node_decl->dump();
-	auto VD = llvm::dyn_cast<clang::VarDecl>(node_decl);
-	
+	llvm::errs()<<"NODE"<<"\n";
+	auto VD = getVarDeclFromDescArray(content, 0);
 	assert(VD);
 	if(VD&&VD->isFunctionOrMethodVarDecl()){
 		llvm::errs()<<"Local Decl\n";
@@ -33,21 +25,16 @@ bool MyASTVisitor::NodeHandler(clang::VarDecl *vdecl)
 		}
 	}
 	{
-	  SL = vdecl->getBeginLoc();
-	  clang::SourceLocation EL = vdecl->getEndLoc();
-	  auto &SM = rew.getSourceMgr();
+	  clang::SourceRange SR = getPragmaSourceRange(vdecl);
 	  std::string codestr;
 	  llvm::raw_string_ostream ss(codestr);
-	  unsigned Line = SM.getSpellingLineNumber(SL);
-	  auto FID = SM.getFileID(SL);
-	  clang::SourceRange SR(SM.translateLineCol(FID, Line, 1),
-				EL);
+	  ss<<"static ";
 	  VD->print(ss);
 	  ss<<";\n";
 	  for(int i = 1; i < dim; i++){
 		  ss<<"static int __XMP_NODES_SIZE_"<<nname<<i<<";\n";
 	  }
-	  
+
 	  for(int i = 0; i < dim; i++){
 		  ss<<"static int __XMP_NODES_RANK_"<<nname<<i<<";\n";
 	  }
