47ff20c26fbd33b90f9f8219b09b2b4c4adf4cc3
Takanori Watanabe <takawata@init-main.com>
2020-12-24 19:29:25 +0900

    Show array subscripts and dim in Align handler as comment.

--- src/AlignHandler.cpp
+++ src/AlignHandler.cpp
@@ -6,8 +6,13 @@ bool MyASTVisitor::AlignHandler(clang::VarDecl *vdecl)
   assert((content));
   auto avdecl = getVarDeclFromDescArray(content, 0);
   auto tvdecl = getVarDeclFromDescArray(content, 1);
-  tvdecl->dump();
+  auto dimstmt = llvm::dyn_cast<clang::IntegerLiteral>(content->getInit(2)
+						       ->IgnoreCasts());
+  int64_t dim = dimstmt->getValue().getSExtValue();
+  dimstmt->dump();
   assert(avdecl);
+  assert(tvdecl);
+  /*Process Aligned variable*/
   AlignedVars.push_back(avdecl);
   std::string Varname = avdecl->getName();
   std::vector <int> NumElems;
@@ -37,12 +42,19 @@ bool MyASTVisitor::AlignHandler(clang::VarDecl *vdecl)
     }
     rew.ReplaceText(SR,  ss.str().c_str());
   }
+  /*Process descriptor*/
   {
     std::string codestr;
     llvm::raw_string_ostream ss(codestr);
     clang::SourceRange SR = getPragmaSourceRange(vdecl);
 
-    ss<<"static void * _XMP_DESC_"<<Varname<<";";
+    ss<<"static void * _XMP_DESC_"<<Varname<<";\n";
+    ss<<"/*Template "<<tvdecl->getName()<<" Dimension "<<dim<<"*/\n";
+    ss<<"/*Array subscripts:";
+    for(auto &it: NumElems){
+      ss<<it<<",";
+    }
+    ss<<"*/\n";
     rew.ReplaceText(SR,  ss.str().c_str());
   }
   return true;
