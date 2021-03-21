586e4524418305f5017cf56962a450f3c9353ce7
Takanori Watanabe <takawata@init-main.com>
2021-03-21 21:12:08 +0900

    Fix Node initialization routine.

--- src/NodeHandler.cpp
+++ src/NodeHandler.cpp
@@ -5,6 +5,7 @@ bool MyASTVisitor::NodeHandler(clang::VarDecl *vdecl)
 	clang::Expr::EvalResult ev;
 	clang::SourceLocation SL;
 	llvm::raw_string_ostream *initstream;
+	std::vector<int> subscripts;
 
 	auto content = llvm::dyn_cast<clang::InitListExpr>(vdecl->getInit());
 	assert((content));
@@ -18,6 +19,7 @@ bool MyASTVisitor::NodeHandler(clang::VarDecl *vdecl)
 	  clang::SourceRange SR = getPragmaSourceRange(vdecl);
 	  std::string codestr;
 	  llvm::raw_string_ostream ss(codestr);
+
 	  initstream = &ss;
 	  if(!VD->isFunctionOrMethodVarDecl()){
 	    initstream = &epistream;
@@ -26,31 +28,37 @@ bool MyASTVisitor::NodeHandler(clang::VarDecl *vdecl)
 	  VD->print(ss);
 	  ss<<";\n";
 
+	  for(int i = 0; i < dim; i++){
+	    if(!content->getInit(i+1)->IgnoreCasts()->EvaluateAsInt(ev, ast)){
+	      content->getInit(i+1)->dump();
+	      llvm::errs()<<"XX";
+	      exit(-1);
+	    }
+	    subscripts.push_back(ev.Val.getInt().getSExtValue());
+	  }
 	  ss<<"/*Dimension"<< dim<<"*/\n";
 	  ss<<"/*Subscripts";
 	  ss<<"*/\n";
-	  for(int i = 1; i < dim; i++){
-		  ss<<"static int __XMP_NODES_SIZE_"<<nname<<i<<";\n";
+	  for(int i = 0; i < dim; i++){
+	    ss<<"static int __XMP_NODES_SIZE_"<<nname<<(dim-i-1);
+	    if(subscripts[i] != -1){
+	      ss <<" = " << subscripts[i];
+	    }
+	    ss <<";\n";
 	  }
 
 	  for(int i = 0; i < dim; i++){
 		  ss<<"static int __XMP_NODES_RANK_"<<nname<<i<<";\n";
 	  }
 	  (*initstream)<<"_XMP_init_nodes_DYNAMIC_GLOBAL(&"<<nname<<","<< dim <<"";
-	  for(int i = content->getNumInits(); i > 1; i--){
-	    if( content->getInit(i-1)->IgnoreCasts()->EvaluateAsInt(ev, ast)){
-	      (*initstream)<<","<<ev.Val.getInt();
-	      llvm::errs()<<"XXX\n";
-	    }else{
-	      llvm::errs()<<"ConvErr"<<"\n";
+	  for(int i = 0; i < dim; i++){
+	    (*initstream)<<","<<subscripts[dim - i -1];
+	    if(subscripts[dim - i - 1] == -1){
+	      (*initstream)<<", & __XMP_NODES_SIZE_"<<nname<<i;
 	    }
 	  }
 	  for(int i = 0; i < dim; i++){
 	    (*initstream)<<", &__XMP_NODES_RANK_"<<nname<<i;
-	  }
-	  
-	  for(int i = 1; i < dim; i++){
-	    (*initstream)<<", &__XMP_NODES_SIZE_"<<nname<<i;
 	  }
 
 	  (*initstream)<<");\n";
